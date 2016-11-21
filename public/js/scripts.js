c933d995c54740ecaf0ccf956627b36e41028d2f
kyleyoung14 <kcyoung@wpi.edu>
2016-11-21 13:36:12 -0500

    first commit

---
+++ public/js/scripts.js
@@ -0,0 +1,25 @@
+// // Add some Javascript code here, to run on the front end.
+// window.onload = function(){
+// 	document.getElementById("Add").onsubmit = handleAdd2
+// 	document.getElementById("Remove").onsubmit = function(){
+// 		console.log("remove query");
+// 	}
+// }
+
+
+
+// function handleAdd2 (submitEvent) {
+//   var name = document.querySelector('input').value
+//   request({
+//     uri: "cs4241-a4-kyleyoung14.herokuapp.com/add",
+//     body: name,
+//     method: "POST"
+//   }, postResponse)
+// }
+
+// function postResponse (err, response, body) {
+//   var statusMessage = document.querySelector('.status')
+//   if (err) return statusMessage.value = err
+//   statusMessage.value = body
+// }
+
---
+++ server.js
@@ -0,0 +1,82 @@
+var http = require('http')
+  , fs   = require('fs')
+  , url  = require('url')
+  , qs   = require('querystring')
+  , prequireort = 8080;
+
+var server = http.createServer (function (req, res) {
+  var uri = url.parse(req.url, true)
+//function sendIndex(res, movieList,timer, query, numRes)
+  switch( uri.pathname ) {
+    // Note the new case handling search
+    case '/':
+      sendFile(res, 'description.txt', 'text/plain')
+      break
+    case '/index.html':
+      sendFile(res, 'description.txt', 'text/plain')
+      break
+    case '/README.md':
+      sendFile(res, 'README.md', 'text/plain')
+      break
+    case '/readme.md':
+      sendFile(res, 'README.md', 'text/plain')
+      break
+    case '/css/style.css':
+      sendFile(res, 'public/css/style.css', 'text/css')
+      break
+    case '/css/bootstrap.min.css':
+      sendFile(res, 'public/css/bootstrap.min.css', 'text/css')
+      break
+    case '/css/bootstrap.min.css.map':
+      sendFile(res, 'public/css/bootstrap.min.css.map', 'text/css')
+      break
+    case '/js/scripts.js':
+      sendFile(res, 'public/js/scripts.js', 'text/javascript')
+      break
+    default:
+      res.end('404 not found')
+  }
+
+})
+
+server.listen(process.env.PORT || port)
+console.log('listening on 8080')
+
+function sendFile(res, filename, contentType) {
+  contentType = contentType || 'text/html'
+
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(200, {'Content-type': contentType})
+    res.end(content, 'utf-8')
+  })
+
+}
+
+
+function sendIndex(res) {
+  var contentType = 'text/html'
+    , html = ''
+
+  html = html + '<html>'
+
+  html = html + '<head>'
+  // You could add a CSS and/or js call here...
+  html = html + '<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">'
+
+  html = html + '<link rel="stylesheet" type="text/css" href="css/style.css">'
+
+  html = html + '<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">'
+
+  html = html + '<link href="https://fonts.googleapis.com/css?family=Luckiest+Guy" rel="stylesheet">'
+
+
+  html = html + '</head>'
+
+  html = html + '<body>'
+
+  html = html + '</body>'
+  html = html + '</html>'
+  
+  res.writeHead(200, {'Content-type': contentType})
+  res.end(html, 'utf-8')
+}
