8d38c84bd811df491de362ad43ba34cfb87a0118
kyleyoung14 <kcyoung@wpi.edu>
2016-12-08 13:49:26 -0500

    browse and sumbit buttons added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,25 +1,9 @@
-// // Add some Javascript code here, to run on the front end.
-// window.onload = function(){
-// 	document.getElementById("Add").onsubmit = handleAdd2
-// 	document.getElementById("Remove").onsubmit = function(){
-// 		console.log("remove query");
-// 	}
-// }
+function chooseFile( fileNum ) {
+   document.getElementById("inp_file"+fileNum).click();
+}
 
 
-
-// function handleAdd2 (submitEvent) {
-//   var name = document.querySelector('input').value
-//   request({
-//     uri: "cs4241-a4-kyleyoung14.herokuapp.com/add",
-//     body: name,
-//     method: "POST"
-//   }, postResponse)
-// }
-
-// function postResponse (err, response, body) {
-//   var statusMessage = document.querySelector('.status')
-//   if (err) return statusMessage.value = err
-//   statusMessage.value = body
-// }
-
+function submitFile( fileNum ) {
+		var fileName = document.getElementById("inp_file"+fileNum)
+		console.log(fileName.value);
+}
